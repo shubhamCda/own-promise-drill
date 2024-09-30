d0b07ce716eeeefd0a2a958553660ebd497a2d6a
Shubham <shubham@gmail.com>
2024-09-30 08:16:03 +0530

    Add problem_02_process function in test_problem_02.cjs

---
+++ test/test_problem_02.cjs
@@ -0,0 +1,50 @@
+const path = require("path")
+
+const { convert_to_uppercase, convert_to_lowercase, sort_file_content, store_filenames, file_reader } = require("../problem_02.cjs");
+
+
+const input_file_path = path.join(__dirname, "../lipsum.txt");
+const uppercase_file_path = path.join(__dirname, "uppercase.txt");
+const lowercase_file_path = path.join(__dirname, "lowercase.txt");
+const sorted_file_path = path.join(__dirname, "sorted.txt");
+const filenames_path = path.join(__dirname, "filenames.txt");
+
+
+function problem_02_process() {
+    file_reader(input_file_path)
+        .then((data) => {
+            return convert_to_uppercase(data, uppercase_file_path);
+
+        })
+        .then((file) => {
+            console.log("uppercase.txt updated successfully..!");
+            return store_filenames(filenames_path, uppercase_file_path);
+
+        })
+        .then(() => {
+            console.log("filenames.txt updated.");
+            return file_reader(uppercase_file_path);
+
+        })
+        .then((data) => {
+            return convert_to_lowercase(data, lowercase_file_path);
+        })
+        .then(() => {
+            console.log("lowercase.txt updated successfull..!");
+            return store_filenames(filenames_path, lowercase_file_path);
+
+        })
+        .then(() => {
+            return file_reader(lowercase_file_path);
+        })
+        .then((data) => {
+            return sort_file_content(data, sorted_file_path);
+        })
+        .then(() => {
+            console.log("sorted.txt updated successfully..!");
+
+        })
+
+}
+
+problem_02_process();
