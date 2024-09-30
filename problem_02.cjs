4721389e99da2d798d46e0727be5bd248ab16ef2
Shubham <shubham@gmail.com>
2024-09-30 08:47:31 +0530

    Add delete_files function

--- problem_02.cjs
+++ problem_02.cjs
@@ -1,65 +1,26 @@
 const fs = require("fs");
-const path = require("path")
 
-const input_file_path = path.join(__dirname, "lipsum.txt");
-const uppercase_file_path = path.join(__dirname, "uppercase.txt");
-const lowercase_file_path = path.join(__dirname, "lowercase.txt");
-const sorted_file_path = path.join(__dirname, "sorted.txt");
-const filenames_path = path.join(__dirname, "filenames.txt");
+/*
+Problem 2:
+    Using own-promise method
 
+*/
 
-function problem_02_process() {
-    file_reader(input_file_path)
-        .then((data) => {
-            return convert_to_uppercase(data, uppercase_file_path);
-
-        })
-        .then((file) => {
-            console.log("uppercase.txt updated successfully..!");
-            return store_filenames(filenames_path, uppercase_file_path);
-
-        })
-        .then(() => {
-            console.log("filenames.txt updated.");
-            return file_reader(uppercase_file_path);
-
-        })
-        .then((data) => {
-            return convert_to_lowercase(data, lowercase_file_path);
-        })
-        .then(() => {
-            console.log("lowercase.txt updated successfull..!");
-            return store_filenames(filenames_path, lowercase_file_path);
-
-        })
-        .then(() => {
-            return file_reader(lowercase_file_path);
-        })
-        .then((data) => {
-            return sort_file_content(data, sorted_file_path);
-        })
-        .then(() => {
-            console.log("sorted.txt updated successfully..!");
-            
-        })
-
-}
-
-
-
-
+// Convert the content to uppercase & write to a new file.
 function convert_to_uppercase(data, filePath) {
     const uppercase_content = data.toUpperCase();
     return file_writer(uppercase_content, filePath);
 }
 
-
+// function to convert newfile to lower case. Then split the contents into sentences. Then write it to a new file.
 function convert_to_lowercase(data, filePath) {
     const lowercase_content = data.toLowerCase();
-    return file_writer(lowercase_content, filePath);
+    const sentences = lowercase_content.split(/(?<=[.!?])\s+/);
+    return file_writer(sentences.join(" "), filePath);
 }
 
 
+// sort the content, write it out to a new file.
 function sort_file_content(data, filePath) {
     const sorted_content = data.split(" ").sort((a, b) => a.localeCompare(b)).join("\n");
 
@@ -67,6 +28,7 @@ function sort_file_content(data, filePath) {
 }
 
 
+// Store the name of the new file in filenames.txt
 function store_filenames(filePath, filesName) {
     return new Promise((resolve, reject) => {
         fs.appendFile(filePath, filesName + "\n", (err) => {
@@ -80,7 +42,7 @@ function store_filenames(filePath, filesName) {
 }
 
 
-
+// function to read the given file.
 function file_reader(filePath) {
     return new Promise((resolve, reject) => {
         fs.readFile(filePath, "utf-8", (err, data) => {
@@ -95,7 +57,7 @@ function file_reader(filePath) {
 
 
 
-
+// function to write file content/ filenames.
 function file_writer(data, filePath) {
     return new Promise((resolve, reject) => {
         fs.writeFile(filePath, data, (err) => {
@@ -109,4 +71,25 @@ function file_writer(data, filePath) {
 }
 
 
-problem_02_process();
+// delete all the new files that are mentioned in that list simultaneously.
+function delete_files(files) {
+    const seperate_files = files.split("\n");
+
+    const deleted_files = seperate_files.forEach(link => {
+        new Promise((resolve, reject) => {
+            if (link !== "") {
+                fs.unlink(link, (err) => {
+                    if (err) {
+                        reject(err);
+                    }else{
+                        resolve("");
+                    }
+                })
+            }
+        })
+    });
+    return Promise.all(deleted_files);
+}
+
+
+module.exports = { convert_to_uppercase, convert_to_lowercase, sort_file_content, store_filenames, file_reader, delete_files };
