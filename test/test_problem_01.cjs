fb4139f19af8c2ccdc4696b9b21d56f14d8da000
Shubham <shubham@gmail.com>
2024-10-02 10:14:32 +0530

    Add main function

--- test/test_problem_01.cjs
+++ test/test_problem_01.cjs
@@ -1,31 +1,11 @@
-const path = require("path");
-
-const { create_directory, generate_json_files, delete_files } = require("../problem_01.cjs");
+const { create_and_delete } = require("../problem_01.cjs");
 
 
 
-const directory_path = path.join(__dirname, "json-files");
-
-
-function create_and_delete(count) {
-    create_directory(directory_path)
-        .then(() => {
-            console.log("Directory created..!");
-            return generate_json_files(directory_path, count);
-
-        })
-        .then((files) => {
-            console.log("json files generated..!");
-            return delete_files(files);
-
-        })
-        .then(() => {
-            console.log("files deleted successully..!");
-
-        })
-
+function main() {
+    
+    create_and_delete(5);
 }
 
+main();
 
-
-create_and_delete(5);
